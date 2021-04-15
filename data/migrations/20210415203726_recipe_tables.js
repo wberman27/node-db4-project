a07d85ea666cb488bcab7c3778111cb47bc8724e
Will B <wberman27@gmail.com>
2021-04-15 17:32:33 -0400

    added migration tables

--- data/migrations/20210415203726_recipe_tables.js
+++ data/migrations/20210415203726_recipe_tables.js
@@ -2,12 +2,49 @@
 exports.up = function(knex) {
   return knex.schema
   .createTable("recipes", tbl =>{
-      tbl.increments("recipe_id")
-      tbl.string("recipe_name", 128).notNullable()
+      tbl.increments("recipe_id") //primary key
+      tbl.string("recipe_name", 128).notNullable() //required
+  })
+  .createTable("ingredients", tbl =>{
+    tbl.increments("ingredient_id")
+    tbl.string("ingredient_name", 128).notNullable()
+  })
+  .createTable("steps", tbl =>{
+      tbl.increments("step_id")
+      tbl.integer("step_number", 128).notNullable()
+      tbl.string("step_instructions", 128).notNullable()
+      //foreign key
+      tbl.integer("recipe_id")
+        .unsigned() //cannot be negative
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT") // for foreign key, use restricted deletion for safety (vs. cascade)
+  })
+  .createTable("StepsIngredients", tbl =>{
+      tbl.increments("StepsIngredients_id")
+      tbl.integer("quantity", 128).notNullable();
+      //foreign key
+      tbl.integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("RESTRICT")
+      //foreign key
+      tbl.integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
   })
 };
 
 exports.down = function(knex) {
   return knex.schema
-  .dropTableIfExists("recipes")
+  .dropTableIfExists("StepsIngredients")
+  .dropTableIfExists("steps")
+  .dropTableIfExists("ingredients")
+  .dropTableIfExists("recipes") //drop last, the tables created first
 };
