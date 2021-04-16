934184926a448c50eef9ca278fcfa8b852b8c813
Will B <wberman27@gmail.com>
2021-04-15 20:08:56 -0400

    added ingredients table and ordering by step number

--- api/model.js
+++ api/model.js
@@ -4,14 +4,18 @@ async function getRecipeById(recipe_id){
     const recipes = await db("recipes as r")
     .leftJoin("steps as s", "r.recipe_id", "s.recipe_id")
     .leftJoin("StepsIngredients as si", "si.step_id", "s.step_id")
+    .leftJoin("ingredients as i", "i.ingredient_id", "si.ingredient_id")
     .select(
         "r.recipe_id",
         "r.recipe_name",
         "s.step_id",
         "s.step_number",
         "s.step_instructions",
-        "si.ingredient_id"
+        "i.ingredient_id",
+        "i.ingredient_name",
+        "si.quantity"
     )
+    .orderBy("s.step_number")
     .where("r.recipe_id", recipe_id)
 
     return recipes
