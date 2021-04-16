129c14dbd401902d10082e651b8ee3f407040029
Will B <wberman27@gmail.com>
2021-04-15 20:52:27 -0400

    fixed error code 404 message. Final.

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -6,11 +6,9 @@ const checkId = async (req, res, next) => {
         if(recipe){
             req.recipe = recipe;
             next();
-        }else{
-            res.status(404).json({message: `404 - Recipe with ID: ${req.params.recipe_id} not found.`})
         }
     }catch(err){
-        res.status(500).json({message: "Error Code 500, "+ err.message})
+        res.status(404).json({message: `404 - Recipe with ID: ${req.params.recipe_id} not found.`})
     }
 }
 
